c1885018453dc593faa4fe5c4c9824472e208c9f
Rehodra <mounasuvra@gmail.com>
2026-03-01 01:39:51 +0530

    last one

--- chat-app/backend/src/routes/user.routes.js
+++
@@ -1,9 +0,0 @@
-import express from 'express';
-import { getUsersForSideBar } from '../controller/user.controller.js';
-import { isLoggedIn } from '../middleware/login.middleware.js';
-
-const router = express.Router();
-
-router.get('/', isLoggedIn, getUsersForSideBar);
-
-export default router;
